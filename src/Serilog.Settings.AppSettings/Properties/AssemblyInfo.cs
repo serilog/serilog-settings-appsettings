fff8374c29010b6e2a466f8e5ac2de67dd0177fd
Richard Webb <richard.webb@helpsystems.com>
2024-07-02 20:08:14 +0100

    Set version to 3.0.0

--- src/Serilog.Settings.AppSettings/Properties/AssemblyInfo.cs
+++ src/Serilog.Settings.AppSettings/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-
 [assembly: CLSCompliant(true)]
 
 [assembly: InternalsVisibleTo("Serilog.Tests, PublicKey=" +
